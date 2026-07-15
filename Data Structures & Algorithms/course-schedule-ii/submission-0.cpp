e1d595c383a83df826fd044237891549d56537ea
Kartik Namdev <namdevkartik72@gmail.com>
2026-07-15 15:43:31 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int n=numCourses;
+        vector<vector<int>> adj(n);
+        for(auto it:prerequisites)
+        {
+            adj[it[1]].push_back(it[0]);
+        }
+        
+        vector<int> indegree(n,0);
+        for(auto it:prerequisites)
+        {
+            indegree[it[0]]++;
+        }
+        queue<int> q;
+        //with all 0 to q
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i]==0) q.push(i);
+        }
+        vector<int> order;
+
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+            order.push_back(node);
+            for(auto it:adj[node])
+            {
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
+            }
+        }
+
+        if(order.size()==n) return order;
+        return {};
+    }
+};
