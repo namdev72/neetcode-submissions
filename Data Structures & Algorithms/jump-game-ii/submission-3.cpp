f2ce0482f742be5448c4083caa36e79b64e3ba08
Kartik Namdev <namdevkartik72@gmail.com>
2026-09-13 16:26:39 +0530

    Add: jump-game-ii - submission-3

---
+++ Data Structures & Algorithms/jump-game-ii/submission-3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        int count=0;
+        int l=0;
+        int r=0;
+        while(r<n-1)
+        {
+            int far=r;
+            for(int i=l;i<=r;i++)
+            {
+                far=max(far,i+nums[i]);
+            }
+            l=r+1;
+            r=far;
+            count++;
+        }
+        return count;
+    }
+};
